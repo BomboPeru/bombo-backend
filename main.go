39ec28c860e238ff9110faec77aa193e4b3c0a65
Bregy Malpartida <bregy.malpartida@utec.edu.pe>
2018-02-27 00:59:43 -0500

    Added cors

--- main.go
+++ main.go
@@ -4,12 +4,22 @@ import (
 	"fmt"
 	"github.com/kataras/iris"
 	"./api"
+	"github.com/iris-contrib/middleware/cors"
 )
 
 const apiVersion = "1.0"
 
 func main() {
 	app := iris.Default()
+
+	crs := cors.New(cors.Options{
+		AllowedOrigins:   []string{"*"}, // allows everything, use that to change the hosts.
+		AllowedMethods: []string{"GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders: []string{"Origin", "Content-Type", "X-Auth-Token", "Authorization"},
+		AllowCredentials: true,
+	})
+
+	app.Use(crs)
 
 	apiParty := app.Party(fmt.Sprintf("/api/v%s", apiVersion))
 
